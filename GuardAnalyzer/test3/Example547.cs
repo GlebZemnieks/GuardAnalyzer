1d2752cc056d6cde44ba068b0a440d7f1c0a56d3
GEZemnieks <Gleb.Zemnieks@paragon-software.com>
2017-05-30 05:35:48 +0300

    Testing data

---
+++ GuardAnalyzer/test3/Example547.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guards;
+
+namespace SonDar.ParagonChallenge.Example
+{
+    class Example547
+    {
+        public void main()
+        {
+            //Example 
+            string test1 = null;
+            string test2 = null;
+
+                    Guard.ArgumentNotNull(() => test1);
+                    Guard.ArgumentNotNull(() => test2);
+                    Guard.ArgumentNotNull(test1, nameof(test1));
+
+                }
+            }
+        }
